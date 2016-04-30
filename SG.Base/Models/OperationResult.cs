bf206b9ee6b1292c6d95fd012175bad88778ffc7
tonghua <tonghua@sectiongo.com>
2016-04-30 00:18:38 -0400

    Initialized

---
+++ SG.Base/Models/OperationResult.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SG.Base.Models {
+    public class OperationResult {
+        #region Members
+
+        #endregion Members
+
+        #region Properties
+        public List<string> Errors { get; set; } = new List<string>();
+        public bool IsSuccess => Errors.Count == 0;
+        public bool IsFail => Errors.Count != 0;
+        #endregion Properties
+
+        #region Constructor
+
+        #endregion Constructor
+
+        #region Methods
+
+        public OperationResult Add(string error) {
+            if (string.IsNullOrEmpty(error)) return this;
+            this.Errors.Add(error);
+            return this;
+        }
+
+        public OperationResult Add(List<string> errors) {
+            this.Errors.AddRange(errors.Where(s=>string.IsNullOrEmpty(s)==false));
+            return this;
+        }
+        public static OperationResult operator +(OperationResult op1, OperationResult op2) {
+            return op1.Add(op2.Errors);
+        }
+        #endregion Methods
+
+        #region Others
+
+        public override string ToString() {
+            return string.Join("\r\n", Errors);
+        }
+
+        #endregion Others
+
+
+
+
+    }
+}
---
+++ UnitTests/Models/OperationResultTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SG.Base.Models;
+
+namespace UnitTests.Models {
+    [TestFixture]
+    public class OperationResultTests {
+        [Test]
+        public void Add1ErrorTest() {
+            // Arrange
+            var op = new OperationResult();
+            // Action
+            op.Add("error1");
+            // Assert
+            Assert.AreEqual(1, op.Errors.Count);
+            Assert.IsTrue(op.IsFail);
+            Assert.IsFalse(op.IsSuccess);
+        }
+
+        [Test]
+        public void AddMultipleErrorsTest() {
+            // Arrange
+            var op = new OperationResult();
+            // Action
+            op.Add("error1").Add("er2").Add("er3");
+            // Assert
+            Assert.AreEqual(3, op.Errors.Count);
+            Assert.IsTrue(op.IsFail);
+            Assert.IsFalse(op.IsSuccess);
+        }
+
+        [TestCase("1", "2", ExpectedResult = 2)]
+        [TestCase("", "", ExpectedResult = 0)]
+        [TestCase("1", "", ExpectedResult = 1)]
+        [TestCase("", "2", ExpectedResult = 1)]
+        public int AddListErrosTest(params string[] errors) {
+            var op = new OperationResult();
+            op.Add(errors.ToList());
+            return op.Errors.Count;
+        }
+        [Test]
+        public void PlusOperatorTest() {
+            // Arrange
+            var op1 = new OperationResult();
+            var op2 = new OperationResult();
+            // Action
+            op1.Add("error1");
+            op2.Add("error2");
+            op1 += op2;
+            // Assert
+            Assert.AreEqual(2, op1.Errors.Count);
+            Assert.IsTrue(op1.IsFail);
+            Assert.IsFalse(op1.IsSuccess);
+        }
+
+        [TestCase("1", "2", ExpectedResult = "1\r\n2")]
+        [TestCase("1", "", ExpectedResult = "1")]
+        [TestCase("", "2", ExpectedResult = "2")]
+        [TestCase("", "", ExpectedResult = "")]
+        public string ToStringTest(params string[] errors) {
+            var op = new OperationResult();
+            foreach (var error in errors){
+                op.Add(error);
+            }
+            return op.ToString();
+        }
+    }
+}
